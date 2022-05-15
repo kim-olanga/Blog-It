d8bafb361e8bbc57ad9030bbc8c6eb477e36b9fc
Kimberly <kimolanga123@gmail.com>
2022-05-15 14:13:17 +0300

    add render template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string
 
 #create flask instance
 app = Flask(__name__)
@@ -6,4 +6,9 @@ app = Flask(__name__)
 #create the first route
 @app.route('/')
 def index():
-    return "<h1>Hello Kimzy</h1>"
+    return render_template("index.html")
+
+#localhost:5000/user/James
+@app.route('/user/<name>')
+def user(name):
+    return "<h1>Hello {}??!!</h1>".format(name)
